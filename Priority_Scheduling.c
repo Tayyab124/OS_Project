a0f159093a0f36c5ed6fc7bc1289c5364eb2340c
Tayyab124 <49874327+Tayyab124@users.noreply.github.com>
2019-04-25 07:13:04 +0530

    Update Priority_Scheduling.c

--- Priority_Scheduling.c
+++ Priority_Scheduling.c
@@ -10,7 +10,7 @@ int main()
   
   int wait_time=0,turnaround_time=0,at[10],bt[10],rt[10]; 
   
-  printf("Enter Total Number of Process:\t "); 
+  printf("Enter Total Process:\t "); 
   
   scanf("%d",&n); 
   
@@ -18,10 +18,10 @@ int main()
   
   for(count=0;count<n;count++) 
   { 
-    printf("Enter Arrival Time for Process, Process Number %d :",count+1); 
+    printf("Enter Arrival Time and Burst Time for Process Process Number %d :",count+1); 
     
     scanf("%d",&at[count]); 
-    printf("Enter Burst Time for Process, Process Number %d :",count+1);
+    
     scanf("%d",&bt[count]); 
     
     rt[count]=bt[count]; 
@@ -53,5 +53,23 @@ int main()
       rt[count]-=time_quantum; 
       time+=time_quantum; 
     } 
-}
+    if(rt[count]==0 && flag==1) 
+    { 
+      remain--; 
+      printf("P[%d]\t|\t%d\t|\t%d\n",count+1,time-at[count],time-at[count]-bt[count]); 
+      wait_time+=time-at[count]-bt[count]; 
+      turnaround_time+=time-at[count]; 
+      flag=0; 
+    } 
+    if(count==n-1) 
+      count=0; 
+    else if(at[count+1]<=time) 
+      count++; 
+    else 
+      count=0; 
+  } 
+  printf("\nAverage Waiting Time= %f\n",wait_time*1.0/n); 
+  printf("Avg Turnaround Time = %f",turnaround_time*1.0/n); 
+  
+  return 0; 
 }
